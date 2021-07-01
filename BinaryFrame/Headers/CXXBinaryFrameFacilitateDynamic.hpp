3e0d940a33b7dfa87516bf9600c3e225203ce77d
AbdullahCanGunkut <60744379+AbdullahCanGunkut@users.noreply.github.com>
2021-07-01 11:32:22 +0300

    Add files via upload

--- BinaryFrame/Headers/CXXBinaryFrameFacilitateDynamic.hpp
+++ BinaryFrame/Headers/CXXBinaryFrameFacilitateDynamic.hpp
@@ -4,13 +4,42 @@
 #include <CXXBinaryFrameTypename.h>
 #include <exception>
 
+
+
+#if defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wterminate"
 #pragma GCC diagnostic ignored "-Wnarrowing"
 #pragma GCC diagnostic ignored "-Wpointer-arith"
 
+#elif defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4267)
+#elif defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wterminate"
+#pragma clang diagnostic ignored "-Wnarrowing"
+#pragma clang diagnostic ignored "-Wpointer-arith"
 
 
+
+
+#endif
+
+
+#define CXX_BINARY_FRAME_FACILITATE_DYNAMIC_NAMESPACE CXX_BINARY_FRAME_NAMESPACE::CXXBinaryFrameFacilitateDynamic
+
+#define FRAME_OBJECT_VECTOR(TYPE , NAME ,  ...) CXX_BINARY_FRAME_NAMESPACE::PointerStorage\
+<CXX_BINARY_FRAME_NAMESPACE::FrameObjectBase>( \
+(CXX_BINARY_FRAME_NAMESPACE::FrameObjectBase*)new CXX_BINARY_FRAME_FACILITATE_DYNAMIC_NAMESPACE::FrameObjectVector<TYPE>(std::vector<TYPE>({__VA_ARGS__}) , NAME))
+
+#define FRAME_OBJECT_ARRAY(...) CXX_BINARY_FRAME_NAMESPACE::PointerStorage\
+<CXX_BINARY_FRAME_NAMESPACE::FrameObjectBase>( \
+(CXX_BINARY_FRAME_NAMESPACE::FrameObjectBase*)new CXX_BINARY_FRAME_FACILITATE_DYNAMIC_NAMESPACE::FrameObjectArray(__VA_ARGS__))
+
+#define FRAME_OBJECT_POINTER(...) CXX_BINARY_FRAME_NAMESPACE::PointerStorage\
+<CXX_BINARY_FRAME_NAMESPACE::FrameObjectBase>( \
+(CXX_BINARY_FRAME_NAMESPACE::FrameObjectBase*)new CXX_BINARY_FRAME_FACILITATE_DYNAMIC_NAMESPACE::FrameObjectPointer(__VA_ARGS__))
 
 
 template <typename T>
@@ -34,12 +63,13 @@ namespace CXXBinaryLoader{
 
 namespace CXXBinaryFrame{
 
+namespace CXXBinaryFrameFacilitateDynamic{
 
 typedef unsigned CountType;
 
 
 template<typename T>
-unsigned CalculateIndexFromIndexArray(const IndexArray& arr) noexcept{
+unsigned CalculateIndexFromIndexArray(const IndexArray& arr) noexcept(false){
 using namespace std::literals::string_literals;
 
 static_assert(std::is_array<T>::value , "T is must be array !" );
@@ -90,20 +120,20 @@ public:
 static_assert(std::is_array<T>::value , "T is must be array type !");
 FrameObjectArray(T arr , const ObjectName& name = "") : Base(arr , name){}
 
-virtual size_type Encode(BinaryFrameBuffer& stack) noexcept{
+virtual size_type Encode(BinaryFrameBuffer& stack) noexcept(false){
 const unsigned long location = stack.size();
 stack.resize(stack.size() + sizeof(T));
 memcpy(&stack[location] , this->obj , sizeof(T));
 return sizeof(T);
 }
 
-virtual size_type Decode(void* ref , unsigned remain) noexcept{
+virtual size_type Decode(void* ref , unsigned remain) noexcept(false){
 CHECK_REMAIN(remain , sizeof(T) , "Index out of Range !");
 memcpy(this->obj , ref , sizeof(T));
 return sizeof(T);
 }
 
-virtual ObjPtr GetObj (const IndexArray& arr = IndexArray()) noexcept{
+virtual ObjPtr GetObj (const IndexArray& arr = IndexArray()) noexcept(false){
 return  ((ObjPtr)this->obj) +
 (arr.size() ? CalculateIndexFromIndexArray<T>(arr) * sizeof(RawType) : 0);
 
@@ -125,7 +155,7 @@ public:
 FrameObjectPointer(T* obj , size_type count , const ObjectName& name = "") : Base(obj , name) , count(count){}
 virtual ~FrameObjectPointer(){if (this->isDestroyable && this->obj)delete [] this->obj;}
 
-virtual size_type Encode(BinaryFrameBuffer& stack) noexcept{
+virtual size_type Encode(BinaryFrameBuffer& stack) noexcept(false){
 if (!this->obj)throw std::runtime_error("Obj is null !");
 
 const unsigned long location = stack.size();
@@ -135,7 +165,7 @@ memcpy(&stack[location + sizeof(size_type)] , this->obj , sizeof(T) * this->coun
 return sizeof(T) * this->count + sizeof(size_type);
 }
 
-virtual size_type Decode(void* ref , unsigned remain) noexcept{
+virtual size_type Decode(void* ref , unsigned remain) noexcept(false){
 
 CHECK_REMAIN(remain , sizeof(size_type) , "Index out of Range ! (count wasn't get)");
 this->count = *(size_type*)ref , remain -= sizeof(size_type);
@@ -152,14 +182,14 @@ memcpy(this->obj , ref + sizeof(size_type) , sizeof(T) * this->count);
 return allocated_size;
 }
 
-virtual ObjPtr GetObj (const IndexArray& arr = IndexArray()) noexcept{
+virtual ObjPtr GetObj (const IndexArray& arr = IndexArray()) noexcept(false){
 if (arr[0] >= this->count)CXX_BINARY_FRAME_THROW(std::out_of_range , "[T*] Index out of Range ! ");
 
 return (ObjPtr)&(this->obj[arr[0]]);
 }
 
-virtual size_type GetSizeof() noexcept{return sizeof(T) * this->count;}
-virtual ExtentArray GetExtent() noexcept{return ExtentArray({this->count});}
+virtual size_type GetSizeof() noexcept(false){return sizeof(T) * this->count;}
+virtual ExtentArray GetExtent() noexcept(false){return ExtentArray({this->count});}
 
 
 protected:
@@ -181,15 +211,12 @@ using Base = FrameObject<std::vector<T>>;
 public:
 typedef std::vector<T> ObjectType;
 
-FrameObjectVector(const ObjectType& obj) : Base(obj){}
+FrameObjectVector(const ObjectType& obj , const ObjectName& name = "") : Base(obj , name){}
 virtual ~FrameObjectVector (){}
 
 
 
-virtual size_type Encode(BinaryFrameBuffer& stack) noexcept{
-if (!this->obj)throw std::runtime_error("Obj is null !");
-
-
+virtual size_type Encode(BinaryFrameBuffer& stack) noexcept(false){
 const
 unsigned count = this->obj.size();
 
@@ -200,7 +227,7 @@ memcpy(&stack[location + sizeof(size_type)] , this->obj.data() , sizeof(T) * cou
 return sizeof(T) * count + sizeof(size_type);
 }
 
-virtual size_type Decode(void* ref , unsigned remain) noexcept{
+virtual size_type Decode(void* ref , unsigned remain) noexcept(false){
 
 CHECK_REMAIN(remain , sizeof(size_type) , "Index out of Range ! (count wasn't get)");
 const
@@ -212,27 +239,38 @@ unsigned allocated_size = sizeof(T) * count;
 CHECK_REMAIN(remain , allocated_size , "Index out of Range !");
 allocated_size += sizeof(size_type);
 
-memcpy(this->obj.data() , ref + sizeof(size_type) , sizeof(T) * count);
+memcpy(this->obj.data() ,  (unsigned char*)ref + sizeof(size_type) , sizeof(T) * count);
 return allocated_size;
 }
 
-virtual ObjPtr GetObj (const IndexArray& arr = IndexArray()) noexcept{
+virtual ObjPtr GetObj (const IndexArray& arr = IndexArray()) noexcept(false){
 if (arr[0] >= this->obj.size())CXX_BINARY_FRAME_THROW(std::out_of_range , "[vector<T>] Index out of Range !");
 return (ObjPtr)&(this->obj[arr[0]]);
 }
 
-virtual size_type GetSizeof() noexcept{return sizeof(T) * this->obj.size();}
-virtual ExtentArray GetExtent() noexcept{return ExtentArray({this->obj.size()});}
+virtual size_type GetSizeof() noexcept(false){return sizeof(T) * this->obj.size();}
+virtual ExtentArray GetExtent() noexcept(false){return ExtentArray({this->obj.size()});}
 
+typename ObjectType::iterator begin (){return this->obj.begin();}
+typename ObjectType::iterator end ()  {return this->obj.end();}
 
 
 };
 
 
-
-
-
+}
 }
 
 }
+
+
+#if defined(__GNUC__)
 #pragma GCC diagnostic pop
+
+#elif defined(_MSC_VER)
+#pragma warning(pop)
+
+#elif defined(__clang__)
+#pragma clang diagnostic pop
+
+#endif
